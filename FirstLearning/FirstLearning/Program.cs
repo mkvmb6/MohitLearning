42672d4cf1f7096dd9812a101309a5734e43fb0b
Mohit <mkvmb.6@gmail.com>
2018-07-26 23:17:24 +0530

    Basics of delegates.

--- FirstLearning/FirstLearning/Program.cs
+++ FirstLearning/FirstLearning/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FirstLearning
 {
+    //Declaration of delegate
+    //<Access> delegate <ReturnType> <DelegateName> (<params> );
+    public delegate void MyDelegate(int x);
+
     class Program
     {
         static void Main(string[] args)
         {
+            var myClass = new MyClass();
+            //myClass.MyNotSoStaticMethod(4);
+
+
+            //Instantiation of delegate
+            var delObj = new MyDelegate(MyClass.MyMethod);
+
+            //Adding methods into it.
+            delObj += myClass.MyNotSoStaticMethod;
+            delObj += myClass.MyNotSoStaticMethod;
+
+            //Removing
+            delObj -= MyClass.MyMethod;
+
+            //Invoking
+            delObj(5);
+
+        }
+
+    }
+
+
+    class MyClass
+    {
+        public int y = 10;
+
+        public static void MyMethod(int a)
+        {
+            Console.WriteLine("MyMethod got called.. with " + a);
+        }
+
+        public void MyNotSoStaticMethod(int y)
+        {
+            Console.WriteLine("MyNotSoStaticMethod got called... with" + y);
         }
     }
 }
