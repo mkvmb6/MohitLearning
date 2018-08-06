234ae3baede790f9ec79ac6c305714dc7f0fed6f
Mohit <mkvmb.6@gmail.com>
2018-08-06 23:01:02 +0530

    Starting of using lambda expressions.

---
+++ FirstLearning/FirstLearning/UseLamdaExpression.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FirstLearning
+{
+    public class UseLamdaExpression
+    {
+        public int GetSquare(int num)
+        {
+            return num * num;
+        }
+    }
+
+    public class LambdaExpressionRunner
+    {
+        static void Main(string[] args)
+        {
+            //num=>num*num
+
+            //var useLambda = new UseLamdaExpression();
+            //var num = 5;
+            //var square = useLambda.GetSquare(num);
+            //Console.WriteLine(square);
+
+
+
+            var useLambda = new UseLamdaExpression();
+            const int num = 5;
+            //Console.WriteLine(new Func<int, int>(useLambda.GetSquare));
+            Console.WriteLine(new Func<int>(() => num * num));
+        }
+    }
+}
