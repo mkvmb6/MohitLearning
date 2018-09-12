bd17a486bb4703d434e2553c9a24a0ba897dda3d
NayanaKamathJ <nayanakmth@gmail.com>
2018-09-12 21:26:44 +0530

    Datastructures->Basics of complexity

---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i < number; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    for (int j = 0; j < number/2; j++)
+            //    {
+            //        Console.WriteLine(j);
+            //    }
+            //}
+
+            for (int i = number; i >0; i=i/2)
+            {
+                Console.WriteLine(i);
+                for (int j = i; j >0; j--)
+                {
+                    Console.WriteLine(j);
+                    
+                }
+            }
+        }
+    }
+}
