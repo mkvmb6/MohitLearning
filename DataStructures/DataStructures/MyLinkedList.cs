b66516daf4b75ce0de31610e54c79b7f1a750cb5
NayanaKamathJ <nayanakmth@gmail.com>
2018-09-14 21:16:08 +0530

    Linked List

---
+++ DataStructures/DataStructures/MyLinkedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+   public class MyLinkedList
+    {
+        private class Node
+        {
+            public int data;
+            public Node next;
+
+        }
+
+        private void Add()
+        {
+           
+        }
+       
+    }
+}
