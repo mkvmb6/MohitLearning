f4983bd6b3e3d406d551b4445f26ef8b79078fa0
Mohit <mkvmb.6@gmail.com>
2018-09-19 22:30:55 +0530

    Add method created in the MyLinkedList class.

--- DataStructures/DataStructures/MyLinkedList.cs
+++ DataStructures/DataStructures/MyLinkedList.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataStructures
+﻿namespace DataStructures
 {
-   public class MyLinkedList
-   {
-       private Node start;
+    public class MyLinkedList
+    {
+        private Node start;
+
         private class Node
         {
             public int data;
@@ -16,11 +11,43 @@ namespace DataStructures
 
         }
 
-        private void Add()
+        public void Add(int data)
         {
-           Node obj = new Node();
+            var newNode = new Node {data = data};
 
+            if (start == null)
+            {
+                start = newNode;
+            }
+            else
+            {
+                var tempNode = start;
+                while (tempNode.next != null)
+                {
+                    tempNode = tempNode.next;
+                }
+                tempNode.next = newNode;
+            }
         }
-       
+
+        public void printList()
+        {
+            //Make sure don't modify where the start is pointing.
+            //Print the whole list using Console.WriteLine();
+        }
+
+    }
+
+    class MyLinkedListRunner
+    {
+        static void Main(string[] args)
+        {
+            var linkedList = new MyLinkedList();
+
+            linkedList.Add(5);
+            linkedList.Add(7);
+            linkedList.Add(8);
+            linkedList.Add(7);
+        }
     }
 }
