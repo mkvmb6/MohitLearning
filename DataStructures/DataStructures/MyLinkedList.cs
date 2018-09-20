649e17a448b4c7de8e245fa1cea019cafe9ba318
Mohit <mkvmb.6@gmail.com>
2018-09-20 23:00:00 +0530

    Delete method implemented.

--- DataStructures/DataStructures/MyLinkedList.cs
+++ DataStructures/DataStructures/MyLinkedList.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace DataStructures
 {
-    public class MyLinkedList
+    public class MyLinkedList<T>
     {
         private Node start;
 
         private class Node
         {
-            public int data;
+            public T data;
             public Node next;
 
         }
 
-        public void Add(int data)
+        public void Add(T data)
         {
             var newNode = new Node {data = data};
 
@@ -32,24 +34,59 @@ namespace DataStructures
             }
         }
 
-        public void printList()
+        public void Print()
         {
             //Make sure don't modify where the start is pointing.
             //Print the whole list using Console.WriteLine();
             if (start == null)
             {
                 Console.WriteLine("List is empty");
+                return;
             }
-            else
+            var tempNode = start;
+            while (tempNode != null)
             {
-                var tempNode = start;
-                while (tempNode!=null)
-                {
-                    Console.Write(tempNode.data+" -> ");
-                    tempNode = tempNode.next;
-                }
+                Console.Write(tempNode.data + " -> ");
+                tempNode = tempNode.next;
             }
 
+            Console.WriteLine();
+
+        }
+
+        public void Delete(T deleteData)
+        {
+            if (start == null)
+            {
+                Console.WriteLine("Node is not present in the list.");
+                return;
+            }
+            if (start.data .Equals( deleteData))
+            {
+                start = start.next;
+                return;
+            }
+            var tempNode = start;
+            var prevNode = start;
+            while (tempNode != null)
+            {
+                if (tempNode.data.Equals( deleteData))
+                {
+                    prevNode.next = tempNode.next;
+                    tempNode.next = null;
+                    Console.WriteLine($"{deleteData} node deleted successfully.");
+                    return;
+                }
+                prevNode = tempNode;
+                tempNode = tempNode.next;
+            }
+            Console.WriteLine($"{deleteData} node is not present in the list.");
+
+        }
+
+        public void Insert(T insertData, int position)
+        {
+            
         }
 
     }
@@ -58,13 +95,18 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            var linkedList = new MyLinkedList();
-
+            var linkedList = new MyLinkedList<int>();
+            
             linkedList.Add(5);
             linkedList.Add(7);
             linkedList.Add(8);
             linkedList.Add(7);
-            linkedList.printList();
+            linkedList.Print();
+            linkedList.Delete(10);
+            linkedList.Delete(8);
+            linkedList.Print();
+            Console.ReadLine();
         }
     }
+    
 }
