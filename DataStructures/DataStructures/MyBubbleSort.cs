2d4ddf755e280718b42cc3bafa0e8a21d79c35d6
NayanaKamathJ <nayanakmth@gmail.com>
2018-10-03 21:47:20 +0530

    BubbleSort Skeleton

---
+++ DataStructures/DataStructures/MyBubbleSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+  public class MyBubbleSort
+    {
+        public static void Main()
+        {
+            var numList = new int[] {4, 7, 1, 9, 4};
+            Do_BubbleSort(numList);
+
+        }
+
+        private static void Do_BubbleSort(int[] numList)
+        {
+            int n = numList.Length;
+
+            int temp=numList[0];
+
+            int max = numList[0];
+            for (int i = 0; i <=n-1; i++)
+            {
+
+                for (int j = 0; j <=n-i-1; j++)
+                {
+                    if ()
+                    {
+                    
+                    }
+                }
+                
+                
+            }
+        }
+    }
+}
