913c7669c8d6006004702dbe05f87176ec41c964
Mohit <mkvmb.6@gmail.com>
2018-10-03 22:25:17 +0530

    Array extension methods added.

---
+++ DataStructures/DataStructures/MyArrayExtesions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public static class MyArrayExtesions
+    {
+        public static void Swap<T>(this T[] array, int pos1, int pos2)
+        {
+            T temp = array[pos1];
+            array[pos1] = array[pos2];
+            array[pos2] = temp;
+        }
+
+        public static void Print<T>(this T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
