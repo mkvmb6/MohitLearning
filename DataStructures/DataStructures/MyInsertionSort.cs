0853e906f0889e6cc6c5b9c84dc1bd11a6e6a3ea
Mohit <mkvmb.6@gmail.com>
2018-10-04 23:09:17 +0530

    Insertion sort half implementation.

---
+++ DataStructures/DataStructures/MyInsertionSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class MyInsertionSort
+    {
+        static void Main(string[] args)
+        {
+            var numList = new int[] { 4, 7, 1, -1, 12, 9, 4 };
+            numList.Print();
+            DoInsertionSort(numList);
+            numList.Print();
+        }
+
+        private static void DoInsertionSort(int[] numList)
+        {
+            int length = numList.Length;
+            int j,temp;
+            for (int i = 0; i < length; i++)
+            {
+                j = i + 1;
+                temp = numList[j];
+                while (j!=-1 && numList[j] < numList[j - 1])
+                {
+                    numList[j] = numList[j-1];
+                    j--;
+                }
+                numList[j] = temp;
+            }
+        }
+    }
+}
