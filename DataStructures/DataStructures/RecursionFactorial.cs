dbf55e66b22c8af180355b80938c6f97ed80cadc
NayanaKamathJ <nayanakmth@gmail.com>
2018-10-08 20:16:14 +0530

    Factorial Program Implementation

---
+++ DataStructures/DataStructures/RecursionFactorial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DataStructures
+{
+   public class RecursionFactorial
+   {
+       private static void Main(string[] args)
+       {
+           Console.WriteLine("Enter a number");
+           var n = Convert.ToInt32(Console.ReadLine());
+           var factorial = Factorial(n);
+
+           Console.WriteLine(factorial);
+       }
+
+       private static int Factorial(int n)
+       {
+           if (n<0)
+           {
+               return 0;
+           }
+           if (n==0)
+           {
+               return 1;
+           }
+           return n * Factorial(n - 1);
+       }
+   }
+}
