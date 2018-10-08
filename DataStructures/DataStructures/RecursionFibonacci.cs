a0ecf60bb12d8545e7a7a91dfcb58a5fdcd45c47
NayanaKamathJ <nayanakmth@gmail.com>
2018-10-08 20:38:00 +0530

    Fibonacci using recursion

---
+++ DataStructures/DataStructures/RecursionFibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class RecursionFibonacci
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            var n = Convert.ToInt32(Console.ReadLine());
+            var fibonacci = Fibonacci(n);
+
+            Console.WriteLine(fibonacci);
+        }
+
+        private static int Fibonacci(int i)
+        {
+            if (i <= 1)
+                return i;
+
+            return Fibonacci(i - 1) + Fibonacci(i - 2);
+        }
+    }
+}
