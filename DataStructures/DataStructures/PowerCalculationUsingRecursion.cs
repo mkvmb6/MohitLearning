d01b09d4bfe19ac7088add6d5ffd5a88b0691de5
NayanaKamathJ <nayanakmth@gmail.com>
2018-10-11 23:05:58 +0530

    Recursion  Power Calculation Using Recursion

---
+++ DataStructures/DataStructures/PowerCalculationUsingRecursion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class PowerCalculationUsingRecursion
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number ");
+            var n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the power");
+            var x = Convert.ToInt32(Console.ReadLine());
+
+            var answer = NPowerX(n, x);
+            Console.WriteLine(answer);
+        }
+
+        private static long NPowerX(int n, int x)
+        {
+            if (x == 0)
+                return 1;
+            return n * NPowerX(n, x - 1);
+        }
+    }
+}
