6f68ca9f6ae7d34d30a3db8e587ad773b527c45a
NayanaKamathJ <nayanakmth@gmail.com>
2018-10-11 22:47:34 +0530

    Log using Recursion

---
+++ DataStructures/DataStructures/LogUsingRecursion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class LogUsingRecursion
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            var n = Convert.ToInt32(Console.ReadLine());
+            //n = int.MaxValue;
+            Console.WriteLine(n);
+            var logNumber = CalculateLog(n);
+
+            Console.WriteLine(logNumber);
+        }
+
+        private static int CalculateLog(int i)
+        {
+            if (i == 1)
+                return 0;
+            return 1 + CalculateLog(i / 2);
+        }
+    }
+}
