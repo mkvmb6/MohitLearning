c45bf81c3aa704574d2036a7915d2b0fe59b8d40
NayanaKamathJ <nayanakmth@gmail.com>
2018-10-12 22:52:24 +0530

    merge sort work

---
+++ DataStructures/DataStructures/MyMergeSort.cs
@@ -0,0 +1,28 @@
+﻿namespace DataStructures
+{
+   public class MyMergeSort
+    {
+        static void Main(string[] args)
+        {
+            var a = new int[] {2, 6, 4, 9, 2, 8};
+            var MergeSortResult = SortByMerge(a);
+        }
+
+        private static int[] SortByMerge(int[] array)
+        {
+            Divide(array);
+        }
+
+        private static void Divide(int[] array)
+        {
+            int length = array.Length;
+            var firstArray = int[length / 2 + 1]{};
+            var secondArray=
+
+
+            if( !array.Length == 1)
+
+            return Divide((array) / 2);
+        }
+    }
+}
