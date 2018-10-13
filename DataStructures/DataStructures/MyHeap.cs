8cff956cb860d6540a41b680c3c1333b688debcd
NayanaKamathJ <nayanakmth@gmail.com>
2018-10-13 21:44:53 +0530

    Heap-> work

---
+++ DataStructures/DataStructures/MyHeap.cs
@@ -0,0 +1,48 @@
+﻿namespace DataStructures
+{
+  public  class MyHeap
+    {
+        private Node root;
+
+        private class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+            
+        }
+
+        public void Add(int data)
+        {
+            var newNode = new Node {data = data};
+
+            if (root==null)
+            {
+                root = newNode;
+                return;
+            }
+            var tempNode = root;
+            while (tempNode.left!=null&& tempNode.right!=null)
+            
+            {
+                
+            }
+
+
+
+
+            if (tempNode.left == null)
+            {
+                tempNode.left = newNode;
+            }
+            else if (tempNode.right == null)
+            {
+                tempNode.right = newNode;
+            }
+            //tempNode=tempNode.
+
+
+        }
+
+    }
+}
