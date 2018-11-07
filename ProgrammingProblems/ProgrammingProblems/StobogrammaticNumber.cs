c5d332fae50ba3baaa55603d687a331887cd94ab
Mohit <mkvmb.6@gmail.com>
2018-11-08 00:22:39 +0530

    StobogrammaticNumber less efficient.

---
+++ ProgrammingProblems/ProgrammingProblems/StobogrammaticNumber.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ProgrammingProblems
+{
+    internal class StobogrammaticNumber
+    {
+
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var stobos = new StobogrammaticNumber().FindStrobogrammatic(16);
+            stopWatch.Stop();
+            /*foreach (var stobo in stobos)
+            {
+                Console.WriteLine(stobo+",");
+            }*/
+            Console.WriteLine(stopWatch.ElapsedMilliseconds);
+        }
+
+        public IList<string> FindStrobogrammatic(int n)
+        {
+
+            #region BadCode
+
+            /*var smallestNumberString = "1";
+            var largestNumberString = "9";
+
+            if (n == 1)
+            {
+                smallestNumberString = "0";
+            }
+            else
+            {
+                for (var i = 1; i < n; i++)
+                {
+                    smallestNumberString += "0";
+                    largestNumberString += "9";
+                }
+            }
+
+            var smallestNumber = Convert.ToInt32(smallestNumberString);
+            var largestNumber = Convert.ToInt32(largestNumberString);
+
+            stoboNumbers.AddRange(GetStobosWithInRange(smallestNumber, 2 * smallestNumber));
+            stoboNumbers.Add("-------------");
+            stoboNumbers.AddRange(GetStobosWithInRange(6 * smallestNumber, 7 * smallestNumber));
+            stoboNumbers.Add("-------------");
+            stoboNumbers.AddRange(GetStobosWithInRange(8 * smallestNumber, largestNumber));
+            stoboNumbers.Add("-------------");*/
+
+            #endregion
+
+            if (n==1)
+            {
+                return GetOneDigitStobos();
+            }
+            return MyFindStrobogrammatic(n).Where(dd => !dd.StartsWith("0")).ToList();
+        }
+
+        private IList<string> MyFindStrobogrammatic(int n)
+        {
+            if (n == 1)
+            {
+                return GetOneDigitStobos();
+            }
+            if (n == 2)
+            {
+                return GetTwoDigitStobos();
+            }
+
+            var twoDigitStobos = GetTwoDigitStobos();
+            var nDigitStobos = new List<string>();
+            foreach (var stobo in twoDigitStobos)
+            {
+                foreach (var nLessTwo in MyFindStrobogrammatic(n - 2))
+                {
+                    nDigitStobos.Add(stobo[0] + nLessTwo + stobo[1]);
+                }
+
+            }
+            return nDigitStobos;//.Where(dd => !dd.StartsWith("0")).ToList();
+        }
+
+
+        private IEnumerable<string> GetStobosWithInRange(int start, int end)
+        {
+            var stobos = new List<string>();
+            for (var i = start; i < end; i++)
+            {
+                var number = i.ToString();
+                if (IsStobogrammatic(number))
+                {
+                    stobos.Add(number);
+                }
+            }
+            return stobos;
+        }
+
+        private bool IsStobogrammatic(string number)
+        {
+            var badNumbers = new[] {'2', '3', '4', '5', '7'};
+            var tempNum = "";
+            var j = 0;
+            for (var i = number.Length-1; i >=0 ; i--)
+            {
+                var c = number[i];
+                if (badNumbers.Any(b => b == c))
+                {
+                    return false;
+                }
+                switch (c)
+                {
+                    case '6':
+                        tempNum += '9';
+                        break;
+                    case '9':
+                        tempNum += '6';
+                        break;
+                    default:
+                        tempNum += c;
+                        break;
+                }
+                if (tempNum[j]!=number[j])
+                {
+                    return false;
+                }
+                j++;
+            }
+            return true;
+        }
+
+        private IList<string> GetOneDigitStobos()
+        {
+            return new List<string> { "0", "1", "8" };
+        }
+        private IList<string> GetTwoDigitStobos()
+        {
+            return new List<string> { "00", "11", "69","88", "96" };
+        }
+    }
+}
