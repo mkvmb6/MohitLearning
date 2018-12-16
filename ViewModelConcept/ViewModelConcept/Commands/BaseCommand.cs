26c4f24dfd5c8a9a0d7b1eebbfe668bde3584595
Mohit <mkvmb.6@gmail.com>
2018-12-16 10:11:20 +0530

    Example code for how to use view models and commands.

---
+++ ViewModelConcept/ViewModelConcept/Commands/BaseCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ViewModelConcept.Commands
+{
+    public class BaseCommand : ICommand
+    {
+        private readonly Action myAction;
+
+        public BaseCommand(Action action)
+        {
+            myAction = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            myAction();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ ViewModelConcept/ViewModelConcept/MyButtonViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using ViewModelConcept.Commands;
+
+namespace ViewModelConcept
+{
+    internal class MyButtonViewModel : INotifyPropertyChanged
+
+    {
+        private string myVmContent = "Mohit";
+
+        public string VmContent
+        {
+            get { return myVmContent; }
+            set
+            {
+                myVmContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int ButtonWidth { get; set; } = 250;
+        public int ButtonHeight { get; set; } = 200;
+        public BaseCommand ChangeWidthHeightCommand {get;}
+
+        public MyButtonViewModel()
+        {
+            ChangeButtonText();
+            ChangeWidthHeightCommand = new BaseCommand(ChangeWidthHeight);
+        }
+
+        private async void ChangeButtonText()
+        {
+            var i = 0;
+            while (i < 1000)
+            {
+                await Task.Delay(200);
+                VmContent = i++.ToString();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void ChangeWidthHeight()
+        {
+            ButtonWidth -= 10;
+            ButtonHeight -= 10;
+            OnPropertyChanged(nameof(ButtonWidth));
+            OnPropertyChanged(nameof(ButtonHeight));
+
+        }
+
+    }
+}
